c89e10bc0a4b9958234d4bca2cda85a6779e604a
Vaibhav Varshney <varshney2vaibhav@gmail.com>
2023-05-20 20:02:02 +0530

    C2

--- src/components/News.js
+++ src/components/News.js
@@ -1,37 +1,92 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+  static defaultProps = {
+    country: 'in',
+    pageSize: 6,
+    category:'general',
+}
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }    
+  
+
     constructor(){
         super();
         
         this.state = {
             articles:[],
-            loading:false
+            loading:false,
+            page:1
     }
 
 }
 
 async componentDidMount() {
-    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=96fb4b8f73bb4ea08f67dd637d56b137";
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=96fb4b8f73bb4ea08f67dd637d56b137&page=1&pageSize=${this.props.pageSize}` ;
+    this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json()
-    this.setState({ articles: parsedData.articles})
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading:false})
+
+}
+
+handlePrev=async() => {
+  let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=96fb4b8f73bb4ea08f67dd637d56b137&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+  this.setState({loading:true});  
+  let data = await fetch(url);
+    let parsedData = await data.json()
+    
+
+    this.setState({
+      page: this.state.page-1,
+      articles: parsedData.articles, 
+      loading:false
+    })
+
+}
+
+handleNext=async() => {
+  
+  let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=96fb4b8f73bb4ea08f67dd637d56b137&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+  this.setState({loading:true});  
+  let data = await fetch(url);
+    let parsedData = await data.json()
+    
+
+    this.setState({
+      page: this.state.page+1,
+      articles: parsedData.articles,
+      loading:false
+    })
+  
 
 }
 
   render() {
     return (
-     <div className="container my-3">        
-        <h2>News Wrapper - Top Headlines</h2>
+     <div className="container my-3">       
+     <h1 className="text-center my-3">News Wrapper - Top Headlines</h1> 
+     {this.state.loading && <Spinner/>}
+       
         <div className="row">
-        {this.state.articles.map((element)=>{
+        {!this.state.loading && this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
             <NewsItem title={element.title?element.title:""} description= {element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} />
             </div>
         })}
             
             
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark"onClick={this.handlePrev}>&larr; Previous</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" class="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,12 +6,12 @@ export class NewsItem extends Component {
     let {title, description , imageUrl, newsUrl} = this.props;
     return (
       <div>
-        <div className="card my-2" style={{width: "18rem"}}>
+        <div className="card my-2" >
             <img src={!imageUrl?"https://lh3.googleusercontent.com/8hRRsq-5RSeLvRv5NAGrQAmrPXTg2vnyYjNIdXM9qhbNCUI0A09T_J3wsFpsSBVsRcRs0-YSVT-gKc6GPqoWwpBPZQ=w128-h128-e365-rj-sc0x00ffffff":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body" >
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>     
       </div>
